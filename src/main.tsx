44bb9bfc4b445244d308f8c1c4ce83404bbbe8fd
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-08-18 21:09:28 -0500

    Added MUI App Bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Styles
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./index.css";
 
+// Routes
 import App from "$routes/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ["decorators-legacy"], // classProperties
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: "$assets", replacement: resolve(__dirname, "src", "assets") },
