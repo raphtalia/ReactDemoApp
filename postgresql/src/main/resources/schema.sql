46cf776153f2f4eabf541a4107907473d6c770cf
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-09-13 21:31:09 -0500

    WIP

--- postgresql/src/main/resources/schema.sql
+++ postgresql/src/main/resources/schema.sql
@@ -1,5 +1,28 @@
-CREATE TABLE IF NOT EXISTS users (
-    id serial PRIMARY KEY,
+CREATE SCHEMA IF NOT EXISTS galadriel;
+
+CREATE TABLE IF NOT EXISTS galadriel.users (
+    user_id serial PRIMARY KEY,
     username VARCHAR(31) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS galadriel.refresh_tokens (
+    token VARCHAR(255) PRIMARY KEY,
+    user_id INT,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES galadriel.users (user_id)
+);
+
+-- Refresh token expiration
+CREATE FUNCTION galadriel.refresh_token_ttl() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    DELETE FROM galadriel.refresh_tokens WHERE timestamp < NOW() - INTERVAL '10 SECOND';
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER refresh_token_ttl_trigger
+    AFTER INSERT ON galadriel.refresh_tokens
+    EXECUTE PROCEDURE galadriel.refresh_token_ttl();
